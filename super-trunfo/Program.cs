675380bd7440a5ae04f50d8f3997ba8018097449
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-03-30 15:22:09 -0300

    create test file

--- super-trunfo/Program.cs
+++ super-trunfo/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//card
-//player
-//jogo
+//carta
+//jogador
+//game
+//baralho
+//fila
+
+
+//32 cartas e um super trunfo
+// 4 a 5 caracteristicas
+// embaralhar e distribuir igualmente
+// pode vê somente a carta de cima
+// um jogar escolhe uma caracteristica e compara com todos os outro jogador, quem ganhar leva todas e colocar no seu monte
+// analisar empate, vai para a proxima rodada, e apenas quem empatou participa, mas é o jogador seguinyr que escolhe a caracteristica
+// apenas carta do tipo A, ganha do super trunfo
+// acaba quando um jogador ficar com todas as cartas
